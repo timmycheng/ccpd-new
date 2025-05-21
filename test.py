a21d6ff909dc50c8e164b4f4c796bf8cfe22198e
tim-home <timmycheng@foxmail.com>
2025-05-22 01:15:14 +0800

    Implement code changes to enhance functionality and improve performance

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import torch
+from ultralytics import YOLO, settings
+
+settings.update({'datasets_dir': './datasets/'})
+print(settings)
+model = YOLO('yolov8s.pt') 
+# model.info()  # print model information
+model.val(data='./datasets/CCPD/splits_test.yaml')
 
 
-
-# Load YOLO model with weights
-model = torch.load('./weights/yolov5/best_yolov5s_final.pt',weights_only=False)
-print(model.yaml)
+# # Load YOLO model with weights
+# model = torch.load('./weights/yolov5/best_yolov5s_final.pt',weights_only=False)
+# print(model.yaml)
